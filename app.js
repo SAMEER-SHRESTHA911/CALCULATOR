39d88a407e33d65c93bb615b15e63b0b8ce151bb
SAMEER SHRESTHA <Nero0293@outlook.com>
2022-07-15 20:41:53 +0545

    Some changes

---
+++ app.js
@@ -0,0 +1,21 @@
+let firstOperand = 0;
+let secondOperand = 0;
+
+const buttons = document.querySelectorAll('.button');
+
+function add()
+{
+
+}
+function subtract()
+{
+
+}
+function multiply()
+{
+
+}
+function divide()
+{
+
+}
